adfa57f13f6797f7df8f0a6137e3a684db26d0e2
VitorHugoAntunes <vitor4ntunes@gmail.com>
2023-04-08 20:01:03 -0300

    Created Header, Carousel products and global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
+import { globalStyles } from '@/styles/global'
+import { Container, Header } from '@/styles/pages/app';
 import type { AppProps } from 'next/app'
+import logoSvg from '../assets/logo.svg'
+
+globalStyles();
+
+import Image from 'next/image'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logoSvg} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,4 @@
+import { getCssText } from '@/styles'
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
@@ -11,6 +12,9 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+
+        {/* Aplicando css da pagina pelo lado do servidor next */}
+        <style dangerouslySetInnerHTML={{ __html: getCssText() }} />
       </body>
     </Html>
   )
