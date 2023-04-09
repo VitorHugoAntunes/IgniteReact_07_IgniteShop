11ce8da0053adf1a9400abf3fe537255e5365397
VitorHugoAntunes <vitor4ntunes@gmail.com>
2023-04-08 22:46:45 -0300

    Listing stripe api products and generating a static page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,21 @@ import Image from "next/image";
 
 import { useKeenSlider } from 'keen-slider/react';
 
-import camiseta1 from '../assets/1.png';
-import camiseta2 from '../assets/2.png';
-import camiseta3 from '../assets/3.png';
-
 import 'keen-slider/keen-slider.min.css';
+import { GetStaticProps } from "next";
+import { stripe } from "@/lib/stripe";
+import Stripe from "stripe";
 
-export default function Home() {
+interface HomeProps {
+  products: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: number;
+  }[]
+}
+
+export default function Home({ products }: HomeProps) {
   // Referencia para a div que contem os itens do carrossel para a funcao alterar o elemento atraves da dom
   const [sliderRef] = useKeenSlider({
     slides: {
@@ -19,32 +27,45 @@ export default function Home() {
   });
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
-      <Product className="keen-slider__slide">
-        <Image src={camiseta1} width={520} height={480} alt="" />
+      {products.map(product => {
+        return (
+          <Product key={product.id} className="keen-slider__slide">
+            <Image src={product.imageUrl} width={520} height={480} alt="" placeholder="blur" blurDataURL={product.imageUrl} />
 
-        <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-
-      <Product className="keen-slider__slide">
-        <Image src={camiseta2} width={520} height={480} alt="" />
-
-        <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-
-      <Product className="keen-slider__slide">
-        <Image src={camiseta3} width={520} height={480} alt="" />
-
-        <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
+            <footer>
+              <strong>{product.name}</strong>
+              <span>{product.price}</span>
+            </footer>
+          </Product>
+        )
+      })}
     </HomeContainer>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await stripe.products.list({
+    expand: ['data.default_price'] // Data antes do atributo pois se trata de uma lista
+  })
+
+  const products = response.data.map(product => {
+    const price = product.default_price as Stripe.Price;
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+      }).format(price.unit_amount! / 100),  // O preco vem em centavos, por isso dividir por 100 - Ex: 1000 centavos = 10 reais
+    }
+  })
+
+  return {
+    props: {
+      products
+    },
+    revalidate: 60 * 60 * 2 // 2 horas - dentro deste intervalo, a pagina fica com conteudo estatico e apenas depois de 2 horas e gerada novamente
+  }
 }
